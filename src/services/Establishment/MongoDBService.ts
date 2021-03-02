d5abaf0a8f0c6df7a0c62f574c7b1045e93cdb16
abdelbassite badou <abdelbassite.badou@gmail.com>
2021-03-03 00:16:40 +0100

    minor type fixes

--- src/services/Establishment/MongoDBService.ts
+++ src/services/Establishment/MongoDBService.ts
@@ -1,6 +1,6 @@
 import { EstablishmentDocument } from '../../types/Establishment'
 export default class MongoDBService {
-    getEstablishment(id: string): Promise<EstablishmentDocument> {
-        return {}
+    getEstablishment(id: string): Promise<EstablishmentDocument | null> {
+        return null
     }
 }
--- src/services/MenuItem/MongoDBService.ts
+++ src/services/MenuItem/MongoDBService.ts
@@ -1,11 +1,11 @@
-import { MenuItem } from '../../types/MenuItem'
+import { MenuItemDocument } from '../../types/MenuItem'
 
 export default class MongoDBService {
     getMenuItem(id: string) {
         return {}
     }
 
-    async getEstablishmentMenuItems(establishmentId: string): Promise<MenuItem[]> {
+    async getEstablishmentMenuItems(establishmentId: string): Promise<MenuItemDocument[]> {
         return []
     }
 }
