571dc752f00ca51c5be8af726d993bcb0684b9e0
abdelbassite badou <abdelbassite.badou@gmail.com>
2021-04-03 22:22:36 +0100

    #1 init owner creation CLI utility ~ parse CLI arguments ( an owner info source json file + an update boolean flag )

---
+++ src/extutils/ownerCRUD/index.ts
@@ -0,0 +1,36 @@
+
+import * as commandLineArgs from 'command-line-args'
+import { Owner } from 'cloud-menu-shared-libs'
+import { readFile } from 'jsonfile'
+import * as pify from 'pify';
+import * as path from 'path';
+
+function parseCLIArgs(): {
+    src: string,
+    update: boolean
+} {
+    const optionDefinitions = [
+        { name: 'src', alias: 'n', type: String },
+        { name: 'update', alias: 'u', type: Boolean }
+    ];
+    var {
+        src,
+        update
+    } = commandLineArgs(optionDefinitions);
+
+
+    return {
+        src,
+        update: update ? true : false
+    }
+}
+
+
+
+async function run() {
+    let { src, update } = parseCLIArgs();
+    if (!src) throw new Error('an owner documents source file is required');
+    const owner: Omit<Owner, '_id' | 'username'> = await pify(readFile)(path.resolve(__dirname, `../../../${src}.json`));
+    if (owner) console.log({ owner });
+}
+run();
